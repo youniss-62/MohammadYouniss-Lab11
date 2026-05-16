d18c50efdf78f85cb30979f8ac075956ccaf43f0
youniss-62 <f24608062@nutech.edu.pk>
2026-05-17 02:48:31 +0500

    Added password reset feature

--- src/com/lab11/git/Main.java
+++ src/com/lab11/git/Main.java
@@ -2,6 +2,6 @@ package com.lab11.git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git Lab");
+        System.out.println("Task 3 completed");
     }
 }
