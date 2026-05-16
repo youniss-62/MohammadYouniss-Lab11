22e6ba18a0ec1db4b2166e78bb08ccd87ee029f9
youniss-62 <f24608062@nutech.edu.pk>
2026-05-17 02:10:10 +0500

    Revert "Added password reset feature"
    This reverts commit 3dc97458f320369c7c6fa6a1a6c8b0711a2ca0e4.

--- src/com/lab11/git/PasswordReset.java
+++
@@ -1,9 +0,0 @@
-package com.lab11.git;
-
-public class PasswordReset {
-
-    public void resetPassword() {
-        System.out.println("Password Reset Feature");
-    }
-
-}
--- src/com/lab11/git/UserAuthentication.java
+++ src/com/lab11/git/UserAuthentication.java
@@ -1,9 +1,5 @@
 package com.lab11.git;
-public class UserAuthentication {
 
-    public void login() {
-        System.out.println("Login Success");
-        System.out.println("User Logged In");
-    }
+public class UserAuthentication {
 
 }
