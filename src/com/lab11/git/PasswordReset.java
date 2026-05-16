3dc97458f320369c7c6fa6a1a6c8b0711a2ca0e4
youniss-62 <f24608062@nutech.edu.pk>
2026-05-17 01:23:57 +0500

    Added password reset feature

---
+++ src/com/lab11/git/PasswordReset.java
@@ -0,0 +1,9 @@
+package com.lab11.git;
+
+public class PasswordReset {
+
+    public void resetPassword() {
+        System.out.println("Password Reset Feature");
+    }
+
+}
--- src/com/lab11/git/UserAuthentication.java
+++ src/com/lab11/git/UserAuthentication.java
@@ -1,5 +1,9 @@
 package com.lab11.git;
-
 public class UserAuthentication {
+
+    public void login() {
+        System.out.println("Login Success");
+        System.out.println("User Logged In");
+    }
 
 }
